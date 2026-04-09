33883e4b01ac6aaf3108042e72458dbf7b23980a
cyphenstyne <cyphenstyne@gmail.com>
2026-04-09 12:44:10 +0530

    added flake.nix

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "Whale Hunter - React Crypto Tracker Environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+  let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in {
+    devShells.${system}.default = pkgs.mkShell {
+      buildInputs = with pkgs; [
+        nodejs_24
+      ];
+
+      shellHook = ''
+        echo "========================================"
+        echo " 🐋 Whale Hunter Environment Active!"
+        echo " Node version: $(node -v)"
+        echo " NPM version:  $(npm -v)"
+        echo "========================================"
+      '';
+    };
+  };
+}
